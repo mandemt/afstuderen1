e62f81caaea5ee3735bffd99659048471d238bbe
mandemt <tomdefilmer@gmail.com>
2024-01-08 18:59:27 +0100

    onzin

--- afstuderen/src/components/FetchData.vue
+++ afstuderen/src/components/FetchData.vue
@@ -1,3 +1,7 @@
 <script setup>
 console.log('fetch data')
 </script>
+<template>
+    <h1>fetchdata.vue component</h1>
+<hr>
+</template>
--- afstuderen/src/views/HomeView.vue
+++ afstuderen/src/views/HomeView.vue
@@ -1,23 +1,89 @@
 <script setup>
-import TheWelcome from '../components/FetchData.vue'
-
-
-// console.log(process.env.VUE_APP_PLANTNET_API_KEY)
-
-let API_KEY = import.meta.env.VITE_API_KEY
-console.log(API_KEY)
-function fetchData() {
- const response = fetch("https://my-api.plantnet.org/v2/species?api-key=" + API_KEY + "&lang=nl")
-const data = response.json
-console.log(data)
-}
+import fetchdata from '../components/FetchData.vue'
+import image from "../img/duizendknoop.png"
 </script>
 
 <template>
+  <h1>Plantnet API. plantensoorten en identificeren</h1>
   <main>
-<button @click="fetchData">klik</button>
-    <TheWelcome />
+    <h2>de data in kaart</h2>
+    <button @click="fetchData">Alle plantnamen</button>
+    <button @click="identifyPlant">identificeer plant</button>
+    <img src="../img/duizendknoop.png">
+
+    <table v-if="kippie">
+      <tr>
+        <td>titel</td>
+      </tr>
+      <tr v-for="lo in kippie">
+        <td>{{ lo.commonNames }}</td>
+
+      </tr>
+    </table>
   </main>
+  <hr>
+  <fetchdata />
 </template>
 
+<script>
+let API_KEY = import.meta.env.VITE_API_KEY
 
+export default {
+
+  data() {
+    return {
+      titles: [],
+      lol: [],
+      kippie: []
+    }
+  },
+  methods: {
+    removeDuplicates(data) {
+      const unique = [...new Map(data.map(item =>
+        [item['id'], item])).values()];
+      this.kippie = unique
+    },
+
+    async fetchData() {
+      let response = await fetch("https://my-api.plantnet.org/v2/species?api-key=" + API_KEY + "&lang=nl")
+      let rawData = await response.json()
+      rawData.forEach(plant => {
+        if (plant.commonNames.length !== 0) {
+          this.lol.push(plant)
+        }
+      })
+      this.removeDuplicates(this.lol)
+    },
+
+    async identifyPlant() {
+
+      const body = new FormData
+body.append("images", image)
+body.append("organs", "leaf")
+      let response = await fetch("https://my-api.plantnet.org/v2/identify/all?include-related-images=false&no-reject=false&lang=nl&type=legacy&api-key=2b10Qo4Dc15tRmkloqUvnqe", {
+        body,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "image/jpeg"
+        },
+        method: "POST"
+      })
+    },
+
+
+
+  }
+}
+
+
+
+
+
+
+</script>
+<style>
+img {
+  width: 100px;
+  height: 100px;
+}
+</style>
